704770c738b5baa01ffbf9387555dd6c599d2cf1
B1gB0 <grand0077@inbox.ru>
2024-04-07 16:06:05 +0300

    Magic Numbers

--- Assets/Scripts/PausePanel.cs
+++ Assets/Scripts/PausePanel.cs
@@ -3,28 +3,34 @@ using UnityEngine.Audio;
 
 public class PausePanel : MonoBehaviour
 {
+    private const string MasterVolume = nameof(MasterVolume);
+    private const string MusicVolume = nameof(MusicVolume);
+    private const string UIVolume = nameof(UIVolume);
+
     [SerializeField] private AudioMixerGroup _mixer;
+    [SerializeField] private float _minVolume = -80f;
+    [SerializeField] private float _maxVolume = 0f;
 
     public void ChangeMasterVolume(float volume)
     {
-        _mixer.audioMixer.SetFloat("MasterVolume", Mathf.Lerp(-80, 0, volume));
+        _mixer.audioMixer.SetFloat(MasterVolume, Mathf.Lerp(_minVolume, _maxVolume, volume));
     }
 
     public void ChangeMusicVolume(float volume)
     {
-        _mixer.audioMixer.SetFloat("MusicVolume", Mathf.Lerp(-80, 0, volume));
+        _mixer.audioMixer.SetFloat(MusicVolume, Mathf.Lerp(_minVolume, _maxVolume, volume));
     }
 
     public void ChangeButtonsVolume(float volume)
     {
-        _mixer.audioMixer.SetFloat("UIVolume", Mathf.Lerp(-80, 0, volume));
+        _mixer.audioMixer.SetFloat(UIVolume, Mathf.Lerp(_minVolume, _maxVolume, volume));
     }
 
     public void ToggleAllSounds(bool enabled)
     {
         if(enabled)
-            _mixer.audioMixer.SetFloat("MasterVolume", 0);
+            _mixer.audioMixer.SetFloat(MasterVolume, _maxVolume);
         else
-            _mixer.audioMixer.SetFloat("MasterVolume", -80);
+            _mixer.audioMixer.SetFloat(MasterVolume, _minVolume);
     }
 }
